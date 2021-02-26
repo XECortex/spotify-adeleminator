383c0983973359da944a94c690763e34fc611743
XECortex <57626963+XECortex@users.noreply.github.com>
2021-02-26 10:37:34 +0100

    Better Spotify detection

--- main.py
+++ main.py
@@ -1,82 +1,97 @@
 #!/usr/bin/env python
 
+import time
 import sys
 import os
 import psutil
 import atexit
 import dbus
 import gi.repository.GLib
+import requests
 from subprocess import check_output
 from dbus.mainloop.glib import DBusGMainLoop
 from subprocess import check_output
 
-# Exit if Spotify is not running
-def exit_if_no_spotify():
-    for runningProcess in psutil.process_iter(['name']):
-        if runningProcess.name() == 'spotify':
-            return True
 
-    print("Spotify not found, exiting")
-    sys.exit(0)
 
-exit_if_no_spotify()
-print('Spotify found!')
+# Version check
+current_version = 0.0
+up_version = float(requests.get('https://raw.githubusercontent.com/XECortex/spotify-adeleminator/main/version').text)
 
-# Get the PulseAudio player ID of spotify
-spotify_pid_list = str(check_output(['pidof', 'spotify'])).replace('\\n\'', '').replace('b\'', '').split(' ')
-sink_list = check_output(['pacmd', 'list-sink-inputs'])
+print(f'Spotify AdEleminator v{current_version}by @XECortex')
 
-print('Spotify PIDs:', spotify_pid_list)
+if up_version > current_version:
+    print('A new version of this software is available')
+    print('Check out "https://github.com/XECortex/spotify-adeleminator"')
 
-current_id = -1
-spotify_id = -1
 
-for line in sink_list.splitlines():
-    if line.startswith(b'    index:'):
-        current_id = str(line)[13:-1]
 
-    for pid in spotify_pid_list:
-        if line.startswith(b'\t\tapplication.process.id = "%b"' % pid.encode('utf8')):
-            spotify_id = current_id
+# Get the PulseAudio player ID of Spotify
+def get_pulse_id():
+    current_id = -1
+    sink_list = check_output(['pacmd', 'list-sink-inputs']).splitlines()
 
-if spotify_id == -1:
-    print("Spotify isn't currently playing anything, exiting")
-    sys.exit(0)
+    for line in sink_list:
+        line = str(line).replace("b'", '').replace("'", '')
+        
+        if line.startswith('    index:'):
+            current_id = line[11:]
+        elif line.endswith('binary = "spotify"'):
+            return current_id
 
-print('Spotify PulseAudio player ID:', spotify_id)
+    return False
+
+while not get_pulse_id():
+    print('Spotify is not running yet. Please open Spotify now and start playing a song')
+    time.sleep(1)
+
+pulse_id = get_pulse_id()
+
+print('Spotify detected')
+print('PulseAudio ID:', pulse_id)
+
+
 
 # Mute and unmute Apotify through the PulseAudio player ID
 def mute():
-    os.system(f'pacmd set-sink-input-mute "{spotify_id}" 1')
+    os.system(f'pacmd set-sink-input-mute "{pulse_id}" 1')
 
 def unmute():
-    os.system(f'pacmd set-sink-input-mute "{spotify_id}" 0')
+    os.system(f'pacmd set-sink-input-mute "{pulse_id}" 0')
 
 # Restore the volume after exiting
 atexit.register(unmute)
 
+
+
 # Connect to the Spotify DBus interface
 print('Connecting to DBus interface...')
-DBusGMainLoop(set_as_default = True)
+
 bus = dbus.SessionBus()
 spotify_bus = bus.get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
 spotify_properties = dbus.Interface(spotify_bus, 'org.freedesktop.DBus.Properties')
 
-# This function will get called whenever the playing song changes
-def properties_changed(bus, message, args):
-    # Get the title and artist of the currently playing song to detect if the currently playing song is an ad
-    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-    title = metadata['xesam:title']
-    artist = metadata['xesam:artist'][0]
+last_title = ""
+last_artist = ""
 
-    if (title == "Advertisement" or title == "Spotify") and not artist:
+print('Everything done! Spotify AdEleminator is now running and listening for ads!')
+
+def song_changed(title, artist):
+    print(title, '-', artist)
+
+    if title in ['Advertisement', 'Ad', 'Spotify', 'spotify']: # and artist == '<Unknown>':
         mute()
     else:
         unmute()
 
-spotify_properties.connect_to_signal('PropertiesChanged', properties_changed)
+while True:
+    # Get the title and artist of the currently playing song to detect if the currently playing song is an ad
+    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    title = metadata['xesam:title'] if metadata['xesam:title'] else '<Unknown>'
+    artist = metadata['xesam:artist'][0] if metadata['xesam:artist'][0] else '<Unknown>'
 
-# Run the GLib event loop to process DBus signals as they arrive
-print('Everything done! Spotify AdEleminator is now running and listening for ads!')
-mainloop = gi.repository.GLib.MainLoop()
-mainloop.run()
+    if title != last_title or artist != last_artist:
+        song_changed(title, artist)
+
+    last_title = title
+    last_artist = artist
